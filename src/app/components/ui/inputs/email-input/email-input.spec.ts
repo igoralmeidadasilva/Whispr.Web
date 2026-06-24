6d4df469421b7ff9e8f7428d7e145bf9c4254999
Igor Almeida <igor.almeidadasilva13@gmail.com>
2026-06-23 21:13:43 -0300

    test: enhance input component tests with ReactiveFormsModule and set inputs

--- src/app/components/ui/inputs/email-input/email-input.spec.ts
+++ src/app/components/ui/inputs/email-input/email-input.spec.ts
@@ -1,19 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EmailInput } from './email-input';
+import { FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
 
 describe('EmailInput', () => {
   let component: EmailInput;
   let fixture: ComponentFixture<EmailInput>;
 
+  const fakeFormGroup = new FormGroup({
+    email: new FormControl(''),
+  });
+
+  const fakeFormGroupDirective = {
+    form: fakeFormGroup,
+  } as FormGroupDirective;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EmailInput],
+      imports: [EmailInput, ReactiveFormsModule],
+      providers: [
+        { provide: FormGroupDirective, useValue: fakeFormGroupDirective }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(EmailInput);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    fixture.componentRef.setInput('label', 'E-mail');
+    fixture.componentRef.setInput('controlName', 'email');
+    fixture.componentRef.setInput('control', fakeFormGroup.get('email'));
+    fixture.componentRef.setInput('errorsMap', new Map<string, string>());
+
+    fixture.detectChanges();
   });
 
   it('should create', () => {
