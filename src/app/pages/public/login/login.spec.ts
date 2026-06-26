5b9c0bf1c341d00503c0e7dbb9f72d5fc15794e1
Igor Almeida <igor.almeidadasilva13@gmail.com>
2026-06-26 00:36:54 -0300

    feat: add router provider to login component tests

--- src/app/pages/public/login/login.spec.ts
+++ src/app/pages/public/login/login.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Login } from './login';
 
+import { provideRouter } from '@angular/router';
+
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
@@ -9,6 +11,9 @@ describe('Login', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Login],
+      providers: [
+        provideRouter([]) 
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Login);
